d44193a439c02afd605a3081b4a049ef33530440
Motunrayo <T0493421@ntu.ac.uk>
2026-02-17 12:44:15 +0000

    Recent improvements

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+class item {
+public:
+    string itemname;
+    string itemtype;
+    int healtheffect;
+    int attackeffect;
+};
 
+class player {
+private:
+    string playersname;
+    int playershealth;
+    int attackpower;
+public:
+    player(string name) {
+        playersname= name;
+
+    }
+    void displayGreeting() {
+        cout<<"\n Welcome" " "<< playersname <<" "  "To Magic World of Gumball";
+    }
+
+
+};
+
+
+class scenario {
+public:
+};
+
+
+int main() {
+    cout<<"Welcome to magic world of gumball!\n";
+    cout<<"\n  Enter player's name: ";
+    string name;
+    getline(cin ,name);
+
+    player p(name);
+    p.displayGreeting();
 }
