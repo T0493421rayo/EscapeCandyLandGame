8bd8319eba14efca7b2b2ff303f575688738c719
Motunrayo <T0493421@ntu.ac.uk>
2026-02-28 19:52:29 +0000

    First update after in-class

--- main.cpp
+++ main.cpp
@@ -4,24 +4,38 @@ using namespace std;
 
 class item {
 public:
-    string itemname;
-    string itemtype;
-    int healtheffect;
-    int attackeffect;
+    string item_name;
+    string item_type;
+    int health_effect;
+    int attack_effect;
 };
 
 class player {
-private:
-    string playersname;
-    int playershealth;
-    int attackpower;
 public:
+    string players_name;
+    int players_health;
+    int attack_power;
+    int life_count;
+
     player(string name) {
-        playersname= name;
+        players_name= name;
+        players_health=100;
+        life_count=5;
+        attack_power=5;
+    }
+
+    void printStatus(){
+        cout<<"\n---PLAYER'S STATUS---" "\n";
+        cout<<"Player's name: "<<players_name<<"\n";
+        cout<<"Player's health: "<<players_health<<"\n";
+        cout<<"Lives : "<<life_count<<"\n";
+        cout<<"Attack power: "<<attack_power<<"\n";
+
+
 
     }
     void displayGreeting() {
-        cout<<"\n Welcome" " "<< playersname <<" "  "To Magic World of Gumball";
+        cout<<"\n---Welcome" " "<< players_name <<" "  "To Magic World of Gumball---";
     }
 
 
@@ -41,4 +55,5 @@ int main() {
 
     player p(name);
     p.displayGreeting();
+    p.printStatus();
 };
