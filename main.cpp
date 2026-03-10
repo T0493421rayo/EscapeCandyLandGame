211a74fe0329fa5731d334da1af54dbb73d9c0a1
Motunrayo <T0493421@ntu.ac.uk>
2026-03-10 11:47:25 +0000

    Sixth update after in-class assessment.

--- main.cpp
+++ main.cpp
@@ -346,8 +346,50 @@ public:
         }
     }
 };
+class Game {
+private:
+    Player player;
+    vector<Scenario*> scenes;
+
+public:
+    Game(const string& playerName) : player(playerName) {
+        Scenario1* s1 = new Scenario1();
+        Scenario2* s2 = new Scenario2();
+        Scenario3* s3 = new Scenario3();
+        Scenario4* s4 = new Scenario4();
+        Scenario5* s5 = new Scenario5();
+        Scenario6* s6 = new Scenario6();
+        Scenario7* s7 = new Scenario7();
+        Scenario8* s8 = new Scenario8();
+        scenes = { s1, s2, s3, s4, s5, s6, s7, s8 };
+    }
 
 
+
+    void start() {
+        cout << "Welcome to the Magic World of Gumball!\n";
+        player.displayGreetings();
+        player.printStatus();
+
+        for (Scenario* s : scenes) {
+            s->run(player);
+
+            if (player.getHealth() <= 0) {
+                cout << "\nYou died! Game over.\n";
+                return;
+            }
+        }
+
+        cout << "\n--- GAME OVER ---\n";
+        player.printStatus();
+
+        if (player.getHealth() > 0) {
+            cout << "Congratulations " << player.getName() << "! You survived!\n";
+        } else {
+            cout << "You did not survive the Magic World of Gumball.\n";
+        }
+    }
+};
 
 int main() {
     cout << "Welcome to the Magic World of Gumball!\n";
@@ -356,44 +398,8 @@ int main() {
     string name;
     getline(cin, name);
 
-    Player p(name);
-
-    p.displayGreetings();
-    p.printStatus();
-
-    Scenario1 s1;
-    Scenario2 s2;
-    Scenario3 s3;
-    Scenario4 s4;
-    Scenario5 s5;
-    Scenario6 s6;
-    Scenario7 s7;
-    Scenario8 s8;
-
-    vector<Scenario*> scenes = {
-        &s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8
-    };
-
-    for (Scenario* s : scenes) {
-        s->run(p);
-
-        if (p.getHealth() <= 0) {
-            cout << "\nYou died! Game over.\n";
-            return 0;
-        }
-    }
-
-
-
-
-    cout << "\n--- GAME OVER ---\n";
-    p.printStatus();
-
-    if (p.getHealth() > 0) {
-        cout << "Congratulations " << p.getName() << "! You survived!\n";
-    } else {
-        cout << "You did not survive the Magic World of Gumball.\n";
-    }
+    Game game(name);
+    game.start();
 
     return 0;
 }
